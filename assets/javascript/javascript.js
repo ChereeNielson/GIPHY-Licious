a3ce1a7eec38c9be2f5f7c470fb457e1ddd7b7c5
Cheree Nielson <38470257+ChereeNielson@users.noreply.github.com>
2018-12-05 13:29:37 -0700

    README update

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,42 +1,39 @@
 $(document).ready(function () {
 
-    // Array with a list of TV sitcoms //
-    var sitcomList = ["Friends", "Will & Grace", "Arrested Development", "Parks and Recreation", "The Office", "Scrubs", "That '70s Show", "3rd Rock from the Sun", "It's Always Sunny in Philadelphia", "Community", "The Big Bang Theory", "Two and a Half Men", "Modern Family", "Seinfeld", "New Girl", "Mr. Bean", "Rules of Engagement",];
+    // Array for list of TV sitcoms //
+    let sitcomList = ["Friends", "Will & Grace", "Arrested Development", "Parks and Recreation", "The Office", "Scrubs", "That '70s Show", "3rd Rock from the Sun", "It's Always Sunny in Philadelphia", "Community", "The Big Bang Theory", "Two and a Half Men", "Modern Family", "Seinfeld", "New Girl", "Mr. Bean", "Rules of Engagement",];
     sitcomList.sort();
 
 
     // Global variable that link to their $counterparts //
-    var btnHolder = $("#sitcomButtons");
-    var button = $(".btn");
-    var mainText = $("#mainText");
+    let sitcomButton = $("#sitcomButton");
+    let mainText = $("#mainText");
 
 
-    // A for loop which takes each item from the array and makes it into a button //
-    for (i = 0; i < sitcomList.length; i++) {
-        var sitcom = $('<button type="button" class="btn btn-success p-2 m-1">').text
-            (sitcomList[i]);
-        sitcom.attr('id', sitcomList[i]);
-        btnHolder.append(sitcom);
-        clickFunction();
+    // Create for loop to capture each sitcom from the array and turn it into a button //
+    for (let i = 0; i < sitcomList.length; i++) {
+        let sitcom = $('<button type="button" class="btn btn-success p-2 m-1">').text(sitcomList[i]);
+           
+        sitcom.attr("sitcom-name", sitcomList[i]);
+        sitcomButton.append(sitcom);
+        
     }
 
 
-    // A function for what happens when you click on a button //
-
-    function clickFunction() {
-        button = $(".btn");
-        button.click(function () {
-            console.log(this.id);
-            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + this.id +"&api_key=3cIsIeloVUjq1nketG9GZn3hRsKSFI3S&limit=10";
-            console.log(queryURL);
-
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function (response) {
-                console.log(response);
-                console.log(response.data[0].embed_url);
+    // Button click function for what happens when you click on a sitcomButton //
+    button.click(function () {
+        mainText.empty();
+        let newSitcom = $(this).attr("sitcom-name");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + newSitcom +"&api_key=3cIsIeloVUjq1nketG9GZn3hRsKSFI3S&limit=10";
+       
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+                
             });
         });
     }
+
+    // Button click function for what happens when you click on the giphy or still //
 });
